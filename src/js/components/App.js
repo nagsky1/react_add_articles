ac77e9ced59050a712cd4f3e0fd4c1cdae4730f0
Nagender Reddy <nagsky1@gmail.com>
2020-11-07 16:14:27 +0530

    add article react code adding

---
+++ src/js/components/App.js
@@ -0,0 +1,18 @@
+// src/js/components/App.js
+import React from "react";
+import Form from "./Form";
+import List from "./List";
+const App = () => (
+    <>
+    <div>
+      <h2>Articles</h2>
+    </div>
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+      <List/>
+    </div>
+  </>
+);
+
+export default App;
